ee7551e4662a707c1410a37b4780d2efb17b4de9
abhijeet <abhijeet40308@gmail.com>
2018-02-01 12:16:49 +0530

    changes

---
+++ placements/assets/js/main.js
@@ -0,0 +1,158 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	});
+    /*---------------------------------------------*
+     * Mobile menu
+     ---------------------------------------------*/
+    $('#navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 40)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+
+
+    /*---------------------------------------------*
+     * Easy Pie Chart
+     ---------------------------------------------*/
+
+
+
+    $('.skills').waypoint(function () {
+        $('.chart').easyPieChart({
+            animate: 2000,
+            scaleColor: false,
+            lineWidth: 4,
+            lineCap: 'square',
+            size: 130,
+            
+            trackColor: false,
+            barColor: '#fff',
+            onStep: function (from, to, percent) {
+                $(this.el).find('.percent').text(Math.round(percent));
+            }
+        });
+    });
+
+    /*---------------------------------------------*
+     * Scroll Up Section
+     ---------------------------------------------*/
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200)
+        {
+            $('.scrollup').fadeIn();
+        }
+        else
+        {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+        return false;
+    });
+
+    /*---------------------------------------------*
+     * Scroll Total Navbar
+     ---------------------------------------------*/
+
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 160
+    });
+	
+	 /*---------------------------------------------*
+     * Timeline Section
+     ---------------------------------------------*/
+	
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+
+
+
+    /*---------------------------------------------*
+     * STICKY scroll
+     ---------------------------------------------*/
+
+//    $.localScroll();
+
+
+
+    /*---------------------------------------------*
+     * Counter 
+     ---------------------------------------------*/
+
+//    $('.statistic-counter').counterUp({
+//        delay: 10,
+//        time: 2000
+//    });
+
+
+
+
+    /*---------------------------------------------*
+     * WOW
+     ---------------------------------------------*/
+
+//        var wow = new WOW({
+//            mobile: false // trigger animations on mobile devices (default is true)
+//        });
+//        wow.init();
+
+
+    /* ---------------------------------------------------------------------
+     Carousel
+     ---------------------------------------------------------------------= */
+
+//    $('.testimonials').owlCarousel({
+//        responsiveClass: true,
+//        autoplay: false,
+//        items: 1,
+//        loop: true,
+//        dots: true,
+//        autoplayHoverPause: true
+//
+//    });
+
+
+    //End
+});
